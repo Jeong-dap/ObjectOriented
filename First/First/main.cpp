c210a8b92c2d878ef5dc3d57da973528909d50b7
Jeong-dap <njd990603@naver.com>
2024-08-05 16:28:16 +0900

    after class registration

---
+++ First/First/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() {
+	int x = 123;	// initialization
+
+	x = 5;	// assignment
+
+	std::cout << x << std::endl;	// endlÀº ÁÙ¹Ù²Þ
+	std::cout << &x << std::endl;
+
+	return 0;
+}
