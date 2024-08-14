f2328d731ee66ee09b2d1840d4acd64b39a6b25d
Jeong-dap <njd990603@naver.com>
2024-08-14 17:16:50 +0900

    after coopang

--- First/First/main.cpp
+++ First/First/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+int add(int x, int y);
 int main() {
 	int x = 123;	// initialization
 
 	x = 5;	// assignment
 
 	std::cout << x << std::endl;	// endlÀº ÁÙ¹Ù²Ş
-	std::cout << &x << std::endl;
+	std::cout << add(x, x) << std::endl;
 
 	return 0;
+}
+int add(int x, int y) {
+	return x + y;
 }
