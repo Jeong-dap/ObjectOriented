b15fc4cfa7e793e54c832587a1711623c35e50a9
Jeong-dap <njd990603@naver.com>
2024-08-14 17:57:11 +0900

    after coopang

---
+++ print&scan/print&scan/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main() {
+	int a, b;
+	std::cout << "정수 a를 입력하시오 : ";
+	std::cin >> a;
+	std::cout << "정수 b를 입력하시오 : ";
+	std::cin >> b;
+	std::cout << a << " + " << b << " = " << a + b << std::endl;
+	return 0;
+}
